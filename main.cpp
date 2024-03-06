39fa53e08ab9546ed4432e3b8072d09f370168c6
Goldman <goldmanmine@mail.ru>
2024-03-06 22:23:43 +0300

    comments

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 
-int A, B, C, D, X, Y, Z;
+int A, B, C, D, X, Y;
 
 
 void square(int &Y);
@@ -12,16 +12,24 @@ int sum(int X, int Y);
 
 void T(int &P, int &L)
 {
+    cout << "P ^ 2 = " << P << " ^ 2 = ";
     X = P;
     square(P);
+    cout << P << endl;
 
+    cout << "L ^ 2 = " << L << " ^ 2 = ";
     X = L;
     square(L);
+    cout << L << endl;
 
+    cout << "P ^ 2 + L ^ 2 = " << P << " + " << L << " = ";
     P = sum(P, L);
+    cout << P << endl;
 
+    cout << "(P ^ 2 + L ^ 2) ^ 2 = " << P << " ^ 2 = ";
     X = P;
     square(P);
+    cout << P << endl;
 }
 
 
@@ -30,11 +38,20 @@ int main()
     cout << "Enter A, B, C, D: ";
     cin >> A >> B >> C >> D;
 
-    T(A, B);
-    T(C, D);
-    Z = sum(A, C);
+    cout << "(A ^ 2 + B ^ 2) ^ 2 + (C ^ 2 + D ^ 2) ^ 2:\n";
 
-    cout << "F = " << Z;
+    cout << "P = A = " << A << "\nL = B = " << B << endl;
+    T(A, B);
+
+    cout << "P = C = " << C << "\nL = D = " << D << endl;
+    T(C, D);
+
+    cout << "(A ^ 2 + B ^ 2) ^ 2 + (C ^ 2 + D ^ 2) ^ 2 = ";
+    cout << A << " + " << C << " = ";
+    A = sum(A, C);
+    cout << A << endl;
+
+    cout << "F = " << A << endl;
 
     return 0;
 }
